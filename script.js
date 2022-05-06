31b1baa82560e8ba32217c8608427941486e2603
Naveen7497 <70712195+Naveen7497@users.noreply.github.com>
2022-05-06 12:34:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ for (var i = 0; i < names.length; i++) {
 	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
         console.log("Goodbye "+ names[i])
 	}
-	/*else{
+	else{
 		console.log("Hello "+ names[i])
-	}*/
+	}
 }
